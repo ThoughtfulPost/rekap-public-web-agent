b386db1f152b1c5d9c94383828260729ee83a99c
Lyndsay Donhoff <lyndsay@thoughtfulpost.com>
2025-02-05 18:02:37 -0800

    Fix JS bundle filename to match Webflow

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index-BdNLGt0P.js', // Forces the same filename
+      }
+    }
+  }
+});
